b93da18492cef12d5933ecabc5ba0c8c2e6c39df
Kirill <fadeevkiri11outlook@gmail.com>
2025-02-11 15:17:47 +0300

    5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,5 @@
 package ru.netology
 
-
-fun getrightTextMinut(seconds:Int) : String{
-    var res = "";
-    var minuts = seconds / 60
-    when {
-        minuts % 10 == 1 && minuts % 100 != 11 -> res = "был(а) ${minuts} минуту назад"
-        (minuts % 10 == 2 || minuts % 10 == 3 || minuts % 10 == 4) && 
-        (minuts % 100 !in 12..14) -> res = "был(а) ${minuts} минуты назад"
-        else -> res = "был(а) ${minuts} минут назад"
-    }
-    return res
-}
-fun getrightTextHours(seconds:Int) : String{
-    var res = "";
-    var hours = seconds / (60 * 60)
-    when(hours) {
-        1 , 21 -> res = "был(а) ${hours} час назад"
-        2 ,3,4, 22 , 23 , 24 -> res = "был(а) ${hours} часа назад"
-        else -> res = "был(а) ${hours} часов назад"
-    }
-    return res
-}
-
-fun agoToText(seconds:Int)  {
-    when(seconds) {
-        in 1..60 -> println("был(а) только что")
-        in 61..60*60-> println(getrightTextMinut(seconds))
-        in 60 * 60 + 1..24 * 60 * 60 -> println(getrightTextHours(seconds))
-        in 24 * 60 * 60+1..24 * 60 * 60*2 -> println("был(а) вчера")
-        in 24 * 60 * 60*2 + 1..24 * 60 * 60*3 -> println("был(а) позавчера")
-        else -> println("был(а) давно")
-    }
-}
-
-// task 2
 fun masercardCommision (transition:Int) : Int {
     val a = transition * 0.006 + 20
     return a.toInt()
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,16 +1,22 @@
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
 import ru.netology.*
 
 class MainKtTest {
+    @Test
+    fun testGetCommission1() {
+        assertEquals("920", getCommission(150000,"Mastercard",200000))
+    }
+    @Test
+    fun testGetCommission2() {
+        assertEquals("920",getCommission(150000,"Mastercard",200000))
+    }
+    @Test
+    fun testGetCommissionBlock() {
+        assertEquals("перевод заблокирован",getCommission(160000))
+        assertEquals("перевод заблокирован",getCommission(50000,"Мир",600000))
+    }
 
-@Test
- fun getCommission() {
-    kotlin.test.assertEquals("470",getCommission(150000,"Mastercard"))
-    kotlin.test.assertEquals("920",getCommission(150000,"Mastercard",200000))
-    kotlin.test.assertEquals("перевод заблокирован",getCommission(200000))
-    kotlin.test.assertEquals("перевод заблокирован",getCommission(50000,"Мир",600000))
- }
+
 }
